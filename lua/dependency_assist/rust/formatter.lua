3e167508f03e20c0547b0cb368207540eb0b3c08
Akin Sowemimo <akin.sowemimo@gmail.com>
2020-11-11 22:43:46 +0000

    Remove some logs and unused functions

--- lua/dependency_assist/rust/formatter.lua
+++ lua/dependency_assist/rust/formatter.lua
@@ -1,18 +1,12 @@
 local M = {}
 
-function M.format_packages(content)
-  print("content:" .. vim.inspect(content))
-  return {}, nil
+function M.update_version(line, version)
+  -- TODO implement me!
+  return ""
 end
 
 function M.format_package_details(name, version)
   return name .. " = " .. string.format('"%s"', version)
-end
-
-function M.update_version(line, version)
-  print("line:" .. vim.inspect(line))
-  print("version:" .. vim.inspect(version))
-  return ""
 end
 
 return M
