84bda3746c0e8e704ee9c59420bd243acd948752
Akin Sowemimo <akin@email.com>
2021-11-11 12:28:54 +0000

    fix: handle compressed API responses

--- lua/dependency_assist/api.lua
+++ lua/dependency_assist/api.lua
@@ -9,20 +9,22 @@ function Api:new(o)
   return o
 end
 
+local decode = vim.json and vim.json.decode or vim.fn.json_decode
+
 --- @param path string
 --- @param cb function
 --- @return number 'the job id'
 function Api:get(path, cb)
   return vim.fn.jobstart(
     string.format(
-      'curl -X GET "%s" -H %s',
+      'curl --compressed -X GET "%s" -H %s',
       self.base_uri .. path,
       '"Content-Type: application/json"'
     ),
     {
       stdout_buffered = true,
       on_stdout = function(_, d, _)
-        local json = vim.fn.json_decode(table.concat(d))
+        local json = decode(table.concat(d))
         cb(json)
       end,
     }
